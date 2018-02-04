2089c28d9fc84949ed4397fa250dc9c9bbbbe58e
leon332157 <leon332157@sina.com>
2018-02-04 14:08:54 -0500

    .

---
+++ module.py
@@ -0,0 +1,17 @@
+#coding=utf8
+import platform
+import os
+if not platform.system() == 'Darwin':
+    print('请在OS X 运行（mac）')
+    exit()
+else:
+    if not os.getuid() == 0:
+        print('需要超级用户（root）权限！')
+        exit()
+    else:
+     pass
+os.system('easy_install pip')
+os.system('pip install bs4')
+os.system('pip install requests')
+os.system('pip install lxml')
+os.system('pip install echarts-python')
